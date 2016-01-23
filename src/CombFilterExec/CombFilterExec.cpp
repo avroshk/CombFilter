c35b03d1ffde786395c94e07d953bc8181d8a460
Chris Latina <chrisrlatina@gmail.com>
2016-01-23 17:56:44 -0500

    Adding cmd line arguments for processing

--- src/CombFilterExec/CombFilterExec.cpp
+++ src/CombFilterExec/CombFilterExec.cpp
@@ -17,16 +17,18 @@ int main(int argc, char* argv[])
     std::string             sInputFilePath,                 //!< file paths
     sOutputFilePath;
     
+    float                   fFIRCoeff           = 0.0;
+    float                   fIIRCoeff           = 0.0;
+    int                     iDelayInMSec        = 0;
+    int                     iSampleRate         = 0;
+    int                     iBlockSize          = 1024;
+    int                     iHopSize            = 512;
+    
     long long               iInFileLength       = 0;        //!< length of input file
-    
     clock_t                 time                = 0;
-    
     float                   **ppfAudioData      = 0;
-    
     CAudioFileIf            *phAudioFile        = 0;
-    
     int                     iNumChannels        = 1;
-    
     CAudioFileIf::FileSpec_t aFileSpec          = {};
     
     showClInfo ();
@@ -35,20 +37,31 @@ int main(int argc, char* argv[])
     // parse command line arguments
     
     // check argc
-    if( argc == 2 ) {
+    if( argc == 4 ) {
         printf("Reading audio file %s\n", argv[1]);
+        printf("\nFIR Coefficient %f",atof(argv[2]));
+        printf("\nIIR Coefficient %f",atof(argv[3]));
+        printf("\nDelay in milliseconds %i",atoi(argv[4]));
+        
+        //Make sure gain is between -1 and 1, a number
+        
+        // Check that delay time is positive and nonzero. Set upper and lower limit 0 - 100ms
+        
     }
-    else if( argc > 2 ) {
+    else if( argc > 4 ) {
         printf("Too many arguments supplied.\n");
         return 0;
     }
     else {
-        printf("Please provide the audio file path.\n");
+        printf("Please provide the audio file path, FIR gain, IIR gain, and delay time in miliseconds.\n");
         return 0;
     }
     
     // get audio file name
     sInputFilePath = argv[1];
+    fFIRCoeff = atof(argv[2]);
+    fIIRCoeff = atof(argv[3]);
+    iDelayInMSec = atoi(argv[4]);
     
     //////////////////////////////////////////////////////////////////////////////
     // open the input wave file
